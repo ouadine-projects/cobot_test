5aa8ffb941380bcc855c776033c6b8db5294849a
LearnRoboticsWROS <ros.master.ai@gmail.com>
2023-08-25 23:37:48 +0200

    made a test_custom_cobot Moveit API

---
+++ src/test_custom_cobot.cpp
@@ -0,0 +1,50 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "move_group_interface_tutorial");
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    sleep(2.0);
+
+    moveit::planning_interface::MoveGroupInterface group("arm");
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    ros::Publisher display_publisher = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+    moveit_msgs::DisplayTrajectory display_trajectory;
+
+    ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
+    ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
+
+    // Target position
+    geometry_msgs::Pose target_pose1;
+    target_pose1.orientation.w = 0;
+    target_pose1.orientation.x = 0;
+    target_pose1.orientation.y = 0;
+    target_pose1.orientation.z = 0;
+
+    target_pose1.position.x = -1;
+    target_pose1.position.y = -1;
+    target_pose1.position.z = 1;
+    group.setPoseTarget(target_pose1);
+
+    // visualize the planning
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    moveit::planning_interface::MoveItErrorCode success = group.plan(my_plan);
+    ROS_INFO("visualizeing plan %s", success.val ? "":"FAILED");
+
+    // move the group arm
+    group.move();
+
+    ros::shutdown();
+    return 0;
+
+}
